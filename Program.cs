2a383f6018a38bf22b3663957c206cd4a8425174
mizterwaffles <nhillhouse04@gmail.com>
2025-12-03 13:33:46 -0700

    Fixed functions to work basically

--- Program.cs
+++ Program.cs
@@ -1,33 +1,60 @@
-﻿class Program
+﻿
+
+class Program
 {
     static void Main()
     {
-        
+        string file = "Mileage.txt";
+        List<TrainingEntry> data = ReadFile(file);
+        Console.WriteLine(data.Count);
+        Options(data, file);
     } 
 
-
-    static void Options(int number)
+    static void Graphing()
+    {
+        
+    }
+    static void Options(List<TrainingEntry> data, string file)
     {
 
-        Console.WriteLine(@"What would you like to do?
+        Console.WriteLine();
+        int number = Getinput(@"What would you like to do?
         1. View Mileage
         2. Input Mileage");
-
         switch (number)
         {
-            case 1:
+            case 1: //View Mileage
+                TrainingEntry Totals = new TrainingEntry() {date = DateTime.Today, mileage = 0, crossTraining = 0};
+                foreach (TrainingEntry entry in data)
+                {
+                    Console.WriteLine($"Date: {entry.date.Date}, Miles: {entry.mileage}, Cross Training: {entry.crossTraining} hrs");
+                    Totals.mileage += entry.mileage;
+                    Totals.crossTraining += entry.crossTraining;
+                }
+                Console.WriteLine($"{Totals.mileage} miles, {Totals.crossTraining} hrs of cross training");
+                
                 break;
-            case 2: 
-                int mileage = Getinput("What was last weeks mileage? ");
-                int crossTraining = Getinput("How long did you spend cross training? ");
-                break;
+                
+
+            case 2: //Input Mileage
+            EnterMileage(data, file);
+            break;
         }
 
+        List<TrainingEntry> EnterMileage(List<TrainingEntry> data, string file)
+        {
+
+            int mileage = Getinput("What was last weeks mileage? ");
+            int crossTraining = Getinput("How long did you spend cross training? ");
+            DateTime date = DateTime.Now;
+
+            data.Add(new TrainingEntry() {date = date, mileage = mileage, crossTraining = crossTraining});
+            WriteFile(data, file);
+            return data;
+        }  
     }
-    static void LoadFile(string filelocation)
-    {
-        string[] file = File.ReadAllLines(filelocation);
-    }
+
+
     static int Getinput(string message)
     {
         bool success = false;
@@ -36,30 +63,42 @@
         {    
             Console.Clear();
             Console.Write(message);
+            Console.WriteLine();
             string? input = Console.ReadLine();
             success = Int32.TryParse(input, out mileage);
         }
         return mileage;
     }
 
+
+
     static List<TrainingEntry> ReadFile(string file)
     {
         List<TrainingEntry> info = new List<TrainingEntry>();
 
         string[] fileinfo = File.ReadAllLines(file);
-        foreach (string line in fileinfo)
+        for (int line = 0; line < fileinfo.Count(); line++)
         {
-            string[] part = line.Split(',');
+            string[] part = fileinfo[line].Split(',');
 
-            info.Add(new TrainingEntry()
-            {
+            info.Add(new TrainingEntry {
                 date = DateTime.Parse(part[0]),
                 mileage = Int32.Parse(part[1]),
                 crossTraining = Int32.Parse(part[2])
-            }
-            );
+            });
+            
         }
         return info;
+    }
+    static void WriteFile(List<TrainingEntry> trainingEntries, string path)
+    {
+        List<string> info = new List<string>();
+        foreach (TrainingEntry line in trainingEntries)
+        {
+            info.Add(line.date.ToString() + ',' + line.mileage.ToString() + ',' + line.crossTraining.ToString());
+        }
+        
+        File.WriteAllLines(path, info);
     }
 
     class TrainingEntry
--- obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+611865d89d161bba2328b1a685759f4de2ce538d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
