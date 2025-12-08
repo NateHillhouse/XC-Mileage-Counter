a25b5ee32ad9080f24d71dde8dc3db43a2d80201
mizterwaffles <nhillhouse04@gmail.com>
2025-12-08 13:48:08 -0700

    Finished graphing, changed it to a bar graph

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ class Program
     static void Main()
     {
         Console.Clear();
-        string file = "Mileage.txt";
+        string file = "Mileage.csv";
         List<TrainingEntry> data = ReadFile(file);
+        //data.Sort();
+        data = data.OrderBy(x => x.date).ToList();
         Options(data, file);
     } 
 
@@ -23,28 +25,45 @@ class Program
         decimal graphHeight = 10;
         decimal graphWidth = 20;
         string[,] graph = new string[mileage.Length, (int)graphHeight];
+
+        //Write each row
         for (int i = 0; i < mileage.Count(); i ++)
         {
+
+            
             for (int j = 0; j < graphHeight; j++) graph[i,j] = " ";
             decimal height = mileage[i] / max * graphHeight - 1;
+
+            
+            //graph[i,(int)height] = "█";
+            for (int x = (int)height; x>=0; x--) graph[i,x] = "█";
+            
+            /*
             int nextItem = data[i].mileage;
             int previousItem = data[i].mileage;
             if (!(i+1 >= data.Count)) nextItem = data[i+1].mileage;
             if (!(i-1 <= 0)) previousItem = data[i-1].mileage;
-            
-            
-            graph[i,(int)height] = "*";
-            if (nextItem < data[i].mileage && previousItem < data[i].mileage) graph[i,(int)height] = "^";
+
+            if (nextItem < data[i].mileage && previousItem < data[i].mileage) graph[i,(int)height] = "█";
             else if (nextItem > data[i].mileage && previousItem > data[i].mileage) graph[i,(int)height] = "v";
             else if (nextItem > data[i].mileage) graph[i,(int)height] = "/";
-            else if (nextItem == data[i].mileage) graph[i,(int)height] = "-";
+            else if (nextItem == data[i].mileage) graph[i,(int)height] = "_";
             else if (nextItem < data[i].mileage) graph[i,(int)height] = "\\";
+            else if (nextItem < data[i].mileage && data[i].mileage < i) graph[i, (int)height] = "|";
+            */
             
         }
         for (int i = (int)graphHeight-1; i >= 0; i--)
             {
-                if (i == graphHeight-1) Console.Write(max + " ");
-                else if (i == (int)(graphHeight-1) /2) Console.Write((int)(graphHeight-1)/2 + " ");
+                if (i == graphHeight-1)
+                {
+                        if (max.ToString().Length == 2) Console.Write(max);
+                    else  Console.Write(max + " ");
+                }
+                else if (i == (int)(graphHeight-1) /2) {
+                if (max.ToString().Length == 2) Console.Write((int)(max)/2);
+                    else Console.Write((int)(max)/2 + " ");
+                }
                 else if (i == 0) Console.Write(0 + " ");
                 else Console.Write("  ");
                 for (int j = 0; j < graph.Length/10; j++)
@@ -63,25 +82,31 @@ class Program
         int number = Getinput(@"What would you like to do?
         1. View Mileage
         2. Input Mileage
-        3. Exit");
+        3. Graph Mileage
+        4. Exit");
         Console.Clear();
+        
+        data = data.OrderBy(x => x.date).ToList();
         switch (number)
         {
             case 1: //View Mileage
             TrainingEntry Totals = new TrainingEntry() {date = DateTime.Today, mileage = 0, crossTraining = 0};
+            string title = "Miles | Cross Training | Date";
+            Console.WriteLine($"{title, 35}");
             foreach (TrainingEntry entry in data)
             {
-                Console.WriteLine($"Date: {entry.date.Date}, Miles: {entry.mileage}, Cross Training: {entry.crossTraining} hrs");
+                Console.WriteLine($"{entry.mileage,3+6} {"|" ,3} {entry.crossTraining,8} {"|" ,7} {entry.date.ToString("MM/dd/yyyy")} ");
                 Totals.mileage += entry.mileage;
                 Totals.crossTraining += entry.crossTraining;
             }
-            Console.WriteLine($"{Totals.mileage} miles, {Totals.crossTraining} hrs of cross training");
+            Console.Write($"Totals");
+            for (int i = 6; i > 0; i--) Console.Write("-");
+            Console.Write("|");
+            for (int i = 16; i > 0; i--) Console.Write("-");
+            Console.WriteLine("|");
+            Console.WriteLine($"{Totals.mileage,3+6} {"|" ,3} {Totals.crossTraining,8} {"|" ,7}");
             Console.WriteLine();
-            Graphing(data);
-            Console.WriteLine();
-            Console.WriteLine("Press any key to continue ");
-            Console.ReadKey(true);
-            Console.Clear();
+            ReturnToMain();
             Options(data, file);
             
             break;
@@ -89,16 +114,33 @@ class Program
 
             case 2: //Input Mileage
             EnterMileage(data, file);
-            Console.WriteLine();
-            Console.WriteLine("Press any key to continue ");
-            Console.ReadKey(true);
-            Console.Clear();
+            ReturnToMain();
             Options(data, file);
             break;
 
             case 3:
+            Graphing(data);
+            ReturnToMain();
+            Options(data, file);
+            break;
+
+            case 4:
 
             break;
+
+            default:
+            Console.Clear();
+            Console.WriteLine("Please Pick a Valid Number. ");
+            Options(data, file);
+            break;
+        }
+        static void ReturnToMain()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue ");
+            Console.ReadKey(true);
+            Console.Clear();
+            
         }
 
         List<TrainingEntry> EnterMileage(List<TrainingEntry> data, string file)
@@ -119,11 +161,20 @@ class Program
     {
         bool success = false;
         int mileage = 0;
+        
+        Console.Write(message);
+        Console.WriteLine();
+        string? input = Console.ReadLine();
+        success = Int32.TryParse(input, out mileage);
+        if (mileage < 1) success = false;
         while (!success) 
         {
+            Console.Clear();
+            Console.WriteLine("Please Pick a Valid Number.");
+            Console.WriteLine();
             Console.Write(message);
             Console.WriteLine();
-            string? input = Console.ReadLine();
+            input = Console.ReadLine();
             success = Int32.TryParse(input, out mileage);
         }
         return mileage;
@@ -134,7 +185,8 @@ class Program
     static List<TrainingEntry> ReadFile(string file)
     {
         List<TrainingEntry> info = new List<TrainingEntry>();
-
+        
+        //info = info.OrderBy(x => x.date).ToList();
         string[] fileinfo = File.ReadAllLines(file);
         for (int line = 0; line < fileinfo.Count(); line++)
         {
@@ -151,12 +203,13 @@ class Program
     }
     static void WriteFile(List<TrainingEntry> trainingEntries, string path)
     {
+        
+        //trainingEntries = trainingEntries.OrderBy(x => x.date).ToList();
         List<string> info = new List<string>();
         foreach (TrainingEntry line in trainingEntries)
         {
             info.Add(line.date.ToString() + ',' + line.mileage.ToString() + ',' + line.crossTraining.ToString());
         }
-        
         File.WriteAllLines(path, info);
     }
 
--- obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b13eb7cc4d62b7e62d3fdeb5032399331cdca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10a148b352ee66e5d2db265a1724b69b06019ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
